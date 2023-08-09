b7452d606395bba78ffb0167a9ef968dbe9bd20c
bluecoder2003 <neelakshidas2003@gmail.com>
2023-08-09 10:50:03 +0530

    Some minor changes

--- js_folder/n.js
+++ js_folder/n.js
@@ -1,7 +1,11 @@
 const typingText = document.getElementById('typing-text');
-const titleText = 'Welcome to SCCSE';
+const typingParagraph1 = document.getElementById('typing-paragraph1');
+
+const titleText = 'SympOsiuM !!';
+const paragraphText1 = 'Welcome to SCCSE';
 
 let titleIndex = 0;
+let paragraphIndex1 = 0;
 
 function typeTitle() {
     typingText.innerHTML += titleText[titleIndex];
@@ -11,4 +15,16 @@ function typeTitle() {
         paragraphInterval1 = setInterval(typeParagraph1, 50);
     }
 }
+
+function typeParagraph1() {
+    typingParagraph1.innerHTML += paragraphText1[paragraphIndex1];
+    paragraphIndex1++;
+    if (paragraphIndex1 === paragraphText1.length) {
+        clearInterval(paragraphInterval1);
+        paragraphInterval2 = setInterval(typeParagraph2, 50);
+    }
+}
+
+let paragraphInterval1;
+
 const titleInterval = setInterval(typeTitle, 100);
